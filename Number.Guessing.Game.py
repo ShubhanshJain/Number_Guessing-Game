e3b46cf4fe33592dc411bed7c0c3cf41a799d561
Shubhansh Jain <93034177+ShubhanshJain@users.noreply.github.com>
2021-11-22 21:52:43 +0530

    Add files via upload

---
+++ Number.Guessing.Game.py
@@ -0,0 +1,23 @@
+''' The aim of project is to create a number guessing game in which the computer itself generate a number and user have to guess it
+in limited number of turns '''
+
+import random, time
+print("Welcome to number guessing game !!!")
+time.sleep(1)
+x = random.randint(1, 101)
+print("The number is generated!!!")
+time.sleep(1)
+print("You have 10 chances to guess")
+count = 10
+while count != 0:
+    a = int(input("Guess the number : "))
+    if a == x:
+        print("You are right, correct number is ", x)
+        print("Congratulations! You won")
+        break
+    elif a > x:
+        print("The number is less than ", a)
+    else:
+        print("The number is greater than ", a)
+    count -= 1
+    
